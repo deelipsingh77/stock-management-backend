395f362f91cab25e42bcb02751621fd7a8196aea
Deelip Kumar Singh <deelipsingh77@gmail.com>
2024-08-31 03:37:04 +0530

    Fixed Error Handline in Routes

--- src/utils/async-handler.js
+++ src/utils/async-handler.js
@@ -4,17 +4,17 @@ const asyncHandler = (fn) => async (req, res, next) => {
   try {
     await fn(req, res, next);
   } catch (error) {
-    if (error.localFilePaths && error.localFilePaths.length > 0) {
-      error.localFilePaths.forEach((path) => {
-        if (fs.existsSync(path)) {
-          fs.unlinkSync(path);
-        }
-      });
-    }
-    res.status(error.status || 500).json({
-      success: false,
-      message: error.message || "Something went wrong",
-    });
+    // if (error.localFilePaths && error.localFilePaths.length > 0) {
+    //   error.localFilePaths.forEach((path) => {
+    //     if (fs.existsSync(path)) {
+    //       fs.unlinkSync(path);
+    //     }
+    //   });
+    // }
+    // res.status(error.status || 500).json({
+    //   success: false,
+    //   message: error.message || "Something went wrong",
+    // });
     next(error);
   }
 };
